aae3d01a02ddffa8abf0be39be34ae405e23001f
arsalanu <arsalanu@AF-ETL-ARSALAN>
2016-11-20 18:49:51 +0500

    update

---
+++ App_Code/UserPosition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for UserPosition
+/// </summary>
+public class UserPosition
+{
+    public string UserName;
+    public float Lat;
+    public float Lon;
+    public int RemainingTime;
+    public UserPosition()
+    {
+    }
+}
